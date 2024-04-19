b3b85ac985e614ab3b309498bca9e1eeed4a61e3
Somdeep Sardar <sardarsomdeep123@gmail.com>
2024-04-19 14:01:32 +0530

    data_transformation and updated the app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from src.logger import logging
 from src.exception import CustomException
-from src.components.data_ingestion import DataIngestion
-from src.components.data_ingestion import DataIngestionConfig
+from src.components.data_ingestion import DataIngestion, DataIngestionConfig
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 import sys
 
 if __name__ == "__main__":
@@ -10,7 +11,10 @@ if __name__ == "__main__":
     try:
         # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
     # Get the configuration for data ingestion
     except Exception as e:
         logging.info("custom exception raised in app.py")
